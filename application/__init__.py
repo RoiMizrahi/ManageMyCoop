7d222e74f7e61581ad89030b1b3086d639d56b4e
Roi Mizrahi <mr.roimiz@Gmail.com>
2022-08-07 16:02:33 +0300

    back

--- application/__init__.py
+++ application/__init__.py
@@ -7,14 +7,14 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-    app.config['SECRET_KEY']= 'mysecret'
+app.config['SECRET_KEY']= 'mysecret'
 
 #######################
 ####DATABASE SETUP#####
 #######################
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xtwcexeekdmmad:b140e5d694ff29d05d5abd747ccd07131d7811f58de83bc99da421c44db5c65b@ec2-50-19-255-190.compute-1.amazonaws.com:5432/d27n0gudtedosa'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
